88349d9932d00c6a1692497f21cd266a1b002daa
Danil138 <playzerou138@mail.ru>
2025-03-04 10:16:43 +0800

    sf

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Your Project Name'
+author = 'Your Name'
+release = '0.1.0'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'alabaster'
+
